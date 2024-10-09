2c4018bc6dc6a673c4fb58a20c4d9c5a3dfc1485
NeethuJohnson139 <neethujohnson139@gmail.com>
2024-10-08 23:37:08 -0400

    Initial Setup

---
+++ employeeconnect/src/components/pages/EmployeeConnectLogin.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../css/Employeeconnect.css";
+
+export const Login = () => {
+  
+  return (
+    <div className="screen-background">      
+        <h1>Employee Connect</h1>
+    </div>        
+  );
+};
