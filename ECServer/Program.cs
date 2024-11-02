f52d4ada671619e2540952d0a71f59853d5eac01
Neethu Johnson <146479838+NeethuJohnson139@users.noreply.github.com>
2024-11-02 12:49:59 -0400

    Update Program.cs

--- ECServer/Program.cs
+++ ECServer/Program.cs
@@ -1,12 +1,28 @@
+using ECServer.Database;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register DataLayer with dependency injection
+builder.Services.AddScoped<DataLayer>();
+// Add CORS policy
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins(allowedOrigins)
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
+// Build the application
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +31,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Use the specified CORS policy
+app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
