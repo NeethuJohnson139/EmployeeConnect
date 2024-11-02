60492e76a3d5393c6fe77414743ad1de0b2dcbf9
Neethu Johnson <146479838+NeethuJohnson139@users.noreply.github.com>
2024-11-02 11:20:54 -0400

    Add Model for Project

---
+++ ECServer/Models/Project.cs
@@ -0,0 +1,12 @@
+namespace ECServer.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
