edcbbf18845deb408ea0fe1e720bf06379e5cb5d
opinder0 <107528251+opinder0@users.noreply.github.com>
2024-11-02 13:44:24 -0400

    Update Department.cs

---
+++ ECServer/Models/Department.cs
@@ -0,0 +1,9 @@
+namespace ECServer.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
