4826212a13edd1755eacecfbdce0d2ea2a2d1ef0
AjayJoy8694 <56147766+AjayJoy8694@users.noreply.github.com>
2024-11-02 13:55:27 -0400

    Add model for Employee

---
+++ ECServer/Models/Emp.cs
@@ -0,0 +1,13 @@
+﻿namespace ECServer.Models
+{
+    public class Emp
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+
+        public string JobTitle { get; set; }
+        public DateTime DateOfJoining { get; set; }
+
+        public string Status { get; set; }
+    }
+}
